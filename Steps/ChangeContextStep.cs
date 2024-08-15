06ac1ca73c4f15084c1d7962834e77e6cb0f0025
mnsternik <mnsternik@gmail.com>
2024-08-15 03:55:49 +0200

    ChangeContextStep.cs created

--- Steps/ChangeContextStep.cs
+++ Steps/ChangeContextStep.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinFormsTestRunner.Core;
+using WinFormsTestRunner.Models;
 
 namespace WinFormsTestRunner.Steps
 {
-    internal class ChangeContextStep
+    internal class ChangeContextStep(GenericStep step) : Step(step.Name, step.Action, step.ElementXPath, step.ElementId, step.BackupScenarioPath)
     {
+        public string ContextId { get; set; } = step.ContextId ?? string.Empty;
+
+        public override void HandleAction()
+        {
+            if (ContextId == "default")
+            {
+                TestRunner.Driver?.SwitchTo().DefaultContent();
+            }
+            else
+            {
+                var iframe = TestRunner.Driver?.FindElement(By.Id(ContextId));
+                TestRunner.Driver?.SwitchTo().Frame(iframe);
+            }
+        }
     }
 }
