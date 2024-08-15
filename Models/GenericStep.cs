fabcb545ce762d0cc479bbcedfc333f9e861fac4
mnsternik <mnsternik@gmail.com>
2024-08-15 03:55:25 +0200

    GenericStep.cs created

--- Models/GenericStep.cs
+++ Models/GenericStep.cs
@@ -23,20 +23,20 @@ namespace WinFormsTestRunner.Models
         public bool? CheckInsideSelect { get; set; } = false;
         public string? BackupScenarioPath { get; set; } = string.Empty;
 
-        //public static Step TransformIntoSpecifedStep(GenericStep step)
-        //{
-        //    return step.Action switch
-        //    {
-        //        "navigate" => new NavigateStep(step),
-        //        "click" => new ClickStep(step),
-        //        "select" => new SelectStep(step),
-        //        "verify" => new VerifyStep(step),
-        //        "iframe-change" => new ChangeContextStep(step),
-        //        "manual" => new ManualStep(step),
-        //        "write" or "write-login" or "write-password" => new WriteStep(step),
-        //        _ => throw new InvalidStepParameterException($"Nieprawidłowy rodzaj akcji ActionType: '{step.Action}'"),
-        //    };
-        //}
+        public static Step TransformIntoSpecifedStep(GenericStep step)
+        {
+            return step.Action switch
+            {
+               "navigate" => new NavigateStep(step),
+               "click" => new ClickStep(step),
+               "select" => new SelectStep(step),
+                "verify" => new VerifyStep(step),
+               "iframe-change" => new ChangeContextStep(step),
+                //"manual" => new ManualStep(step),
+                "write" or "write-login" or "write-password" => new WriteStep(step),
+                _ => throw new InvalidStepParameterException($"Nieprawidłowy rodzaj akcji ActionType: '{step.Action}'"),
+            };
+        }
 
     }
 }
