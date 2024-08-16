915394049e12f0dcce7e80ed91fb7872e75be038
mnsternik <mnsternik@gmail.com>
2024-08-16 04:19:55 +0200

    Validation added

--- Configuration/ConfigManager.cs
+++ Configuration/ConfigManager.cs
@@ -5,36 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 using WinFormsTestRunner.Utilities;
 using WinFormsTestRunner.Models;
-using Microsoft.VisualBasic.ApplicationServices;
+using WinFormsTestRunner.Exceptions;
 
 namespace WinFormsTestRunner.Configuration
 {
     internal class ConfigManager
     {
-        public static string DriverPath { get; set; } = string.Empty;
-        public static string FirefoxPath { get; set; } = string.Empty;
-        public static string LogsFolderPath { get; set; } = string.Empty;
-        public static int ElementWaitingTimeout { get; set; } = 0;
+        private static Config? _config;
 
-        
+        public static Config Config
+        {
+            get => _config ?? new Config();
+            set
+            {
+                ValidateConfig(value);
+                _config = value;
+            }
+        }
+
         private static readonly string ConfigFileName = "config.json";
         //private static readonly string ConfigFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configuration", ConfigFileName);
         private static readonly string ConfigFilePath = Path.Combine("C:\\Users\\mnste\\OneDrive\\Pulpit\\TestRunnerData", "Configuration", ConfigFileName);
 
         public static void LoadConfig()
         {
-            var config = JSONFileHandler.Deserialize<Config>(ConfigFilePath);
-
-            DriverPath = config.DriverPath;
-            FirefoxPath = config.FirefoxPath; 
-            LogsFolderPath = config.LogsFolderPath;
-            ElementWaitingTimeout = config.ElementWaitingTimeout;
+            Config = JSONFileHandler.Deserialize<Config>(ConfigFilePath);
         }
 
-        public static void SaveConfig() 
+        public static void SaveConfig()
         {
-            Config config = new Config(DriverPath, FirefoxPath, LogsFolderPath, ElementWaitingTimeout);
-            JSONFileHandler.Serialize(config, ConfigFilePath);
+            JSONFileHandler.Serialize(Config, ConfigFilePath);
+        }
+
+        private static void ValidateConfig(Config config)
+        {
+            if (config == null)
+            {
+                throw new ConfigException($"Plik konfiguracyjny jest pusty lub nie istnieje. Sprawdzana lokalizacja: '{ConfigFilePath}'");
+            }
+            if (string.IsNullOrEmpty(config.DriverPath) || !File.Exists(config.DriverPath))
+            {
+                throw new ConfigException($"Wskazano błędną ścieżke do pliku geckodriver.exe w pliku konfiguracyjnym: '{config.DriverPath}'");
+            }
+            if (string.IsNullOrEmpty(config.FirefoxPath) || !File.Exists(config.FirefoxPath))
+            {
+                throw new ConfigException($"Wskazano błędną ścieżke do pliku firefox.exe w pliku konfiguracyjnym: '{config.FirefoxPath}'");
+            }
+            if (string.IsNullOrEmpty(config.LogsFolderPath) || !Directory.Exists(config.LogsFolderPath))
+            {
+                throw new ConfigException($"Wskazano błędną ścieżke do folderu, w którym mają tworzyć się logi: '{config.LogsFolderPath}'");
+            }
+            if (config.ElementWaitingTimeout <= 0)
+            {
+                throw new ConfigException($"Czas oczekiwania na elementy na stronie musi być większy niż 0, aktualna wartość: '{config.ElementWaitingTimeout}'");
+            }
         }
     }
 }
