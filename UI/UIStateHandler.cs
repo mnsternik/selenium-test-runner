9fcb8e364e814e5d68e2489d4dd503d69874eb50
mnsternik <mnsternik@gmail.com>
2024-08-23 19:17:51 +0200

    Handling test status added

---
+++ UI/UIStateHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTestRunner.UI
+{
+    internal static class UIStateHandler
+    {
+        private static readonly Dictionary<string, bool> ButtonStates = new Dictionary<string, bool>();
+
+        public static event Action<string, bool>? ButtonStateChanged;
+        public static event Action<string> TestStatusChanged; 
+
+        static UIStateHandler()
+        {
+            InitializeButtonStates();
+        }
+        // Ustawienie początkowego stanu aktywności przycisków
+        private static void InitializeButtonStates()
+        {
+            // Testing tab control
+            ButtonStates["StartTestButton"] = true;
+            ButtonStates["StopTestButton"] = false;
+            ButtonStates["EndTestButton"] = false;
+            ButtonStates["NextStepButton"] = false;
+            ButtonStates["RetryStepButton"] = false;
+            ButtonStates["ScenarioPathButton"] = true;
+
+            // Settings tab control
+        }
+
+        public static void SetButtonState(string buttonName, bool isEnabled)
+        {
+            if (!ButtonStates.TryAdd(buttonName, isEnabled))
+            {
+                ButtonStates[buttonName] = isEnabled;
+            }
+
+            ButtonStateChanged?.Invoke(buttonName, isEnabled);
+        }
+
+        public static bool GetButtonState(string buttonName)
+        {
+            return ButtonStates.TryGetValue(buttonName, out var isEnabled) && isEnabled;
+        }
+
+        public static void SetTestStatus(string status)
+        {
+            TestStatusChanged.Invoke(status); 
+        }
+    }
+}
