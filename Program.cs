a0f971db2fc5025de539352a0ed7d8393568c41c
mnsternik <mnsternik@gmail.com>
2024-08-28 22:10:03 +0200

    Global exception handling added

--- Program.cs
+++ Program.cs
@@ -1,17 +1,39 @@
+using WinFormsTestRunner.Configuration;
+using WinFormsTestRunner.Utilities;
+
 namespace WinFormsTestRunner
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm());
+        }
+
+        // Handles UI thread exceptions
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            HandleException(e.Exception);
+        }
+
+        // Handles non-UI thread exceptions
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            HandleException(e.ExceptionObject as Exception);
+        }
+
+        static void HandleException(Exception ex)
+        {
+            if (!string.IsNullOrEmpty(ConfigManager.Config.LogsFolderPath))
+            {
+                Logger.Log($"Wyst¹pi³ nieoczekiwany b³¹d: {ex.Message}"); // or write only to file?
+            }
+            MessageBox.Show($"Wyst¹pi³ nieoczekiwany b³¹d: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
