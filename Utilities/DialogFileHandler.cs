903f09ebaf256ca0f9fd3556ab31b250bf72e7b6
mnsternik <mnsternik@gmail.com>
2024-09-30 11:10:52 +0200

    Checking if initial direcotry exists added

--- Utilities/DialogFileHandler.cs
+++ Utilities/DialogFileHandler.cs
@@ -9,10 +9,19 @@ namespace WinFormsTestRunner.Utilities
 {
     internal class DialogFileHandler
     {
-        public static string GetFilePath(string fileTypeFilter)
+        public static string GetFilePath(string fileTypeFilter, string? initialDirectory = null)
         {
             using OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (initialDirectory != null && Directory.Exists(initialDirectory))
+            {
+                openFileDialog.InitialDirectory = initialDirectory;
+            }
+            else
+            {
+                openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            }
+
             openFileDialog.Filter = fileTypeFilter;
             openFileDialog.FilterIndex = 1;
             openFileDialog.RestoreDirectory = true;
